1e7b8d2906d2c7e2e562379eaaa5e1f0594b09fd
viet.nguyenxuan <viet.nguyenxuan@nttdata.com>
2021-10-29 22:17:13 +0700

    Config API KEY BSCScan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import {task} from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 
 const mnemonic = "crew apology rather fog art mansion price boost valve have bring concert";
-const bscscanApiKey = "";
+const bscscanApiKey = "RF6Y2YFI6M9EQB75BYHAZMXMKFPU883V7Z";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
